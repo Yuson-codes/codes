40bc5b9aa6aecf23205e7d037a4fe27932172d61
Yuson-codes <ikescodes@gmail.com>
2025-06-15 14:26:52 +0800

    Add files via upload

---
+++ files/output.js
@@ -0,0 +1,71 @@
+let expression = "";
+let displayValue = "";
+
+function showNumber(num) {
+    expression += num;
+    displayValue += num;
+    updateDisplay();
+}
+
+function showSymbol(symbol) {
+    if (symbol === '%') {
+        expression += "/100";
+        displayValue += '%'
+    } 
+    else if (symbol === '(') {
+        let lastChar = expression.slice(-1);
+        if (/\d|\)/.test(lastChar)) {
+            expression += '*(';
+            displayValue += '(';
+        }
+        else {
+            expression += '(';
+            displayValue += '(';
+        }
+    }
+    else {
+        expression += symbol;
+        displayValue += symbol;
+    }
+    updateDisplay();
+}
+
+function showSign(oparator) {
+    if (oparator === 'x') {
+        expression += '*';
+        displayValue +='x';
+    }
+    else if (oparator === '÷') {
+        expression += '/';
+        displayValue += '÷';
+    }
+    else {
+        expression += oparator;
+        displayValue += oparator;
+    }
+    updateDisplay();
+}
+
+function showTotal() {
+    try {
+        let result = eval(expression);
+        expression = result.toString();
+        displayValue = result.toString();
+        updateDisplay();
+    }
+    catch (e) {
+        expression = "Error";
+        displayValue = "Error";
+        updateDisplay();
+    }
+}
+
+function erasenumber() {
+    expression = "";
+    displayValue = "";
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementsByClassName("box")[0].textContent = displayValue;
+}
